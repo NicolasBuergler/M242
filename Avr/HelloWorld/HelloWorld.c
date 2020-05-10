a7bdbd42929d044382bc3735432b47e688b26bac
Nicolas Bürgler <trajan15.nb@gmail.com>
2020-05-10 16:27:08 +0200

    BlinkingLED1 SetRegister hinzugefügt

--- Avr/HelloWorld/HelloWorld.c
+++ Avr/HelloWorld/HelloWorld.c
@@ -9,16 +9,12 @@
 int main(void)
 {
 	Usart_Init(250000);
-	//Setzt die Datenflussrichtung vom Pin 5 vom Port B auf Output 
-	SetRegister(PortB.DDR, (PIN5, DdrOutput));
 
 	while (True)
 	{
-		//LED einschalten
-		_delay_ms(1000);
-		//LED Ausschalten
+		TRACE("Hello World");
 		_delay_ms(1000);
 	}
-
+	
 	return 0;
 }
